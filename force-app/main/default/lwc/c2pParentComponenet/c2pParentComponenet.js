092f5e34189c3c9f7d9e34423122c4ce9ffbc303
Manas <manasdatta.cs@gmail.com>
2025-05-19 19:05:35 +0530

    commit 16

--- force-app/main/default/lwc/c2pParentComponenet/c2pParentComponenet.js
+++ force-app/main/default/lwc/c2pParentComponenet/c2pParentComponenet.js
@@ -1,18 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class C2pParentComponenet extends LightningElement {
+  showModal = false;
 
-    showModal=false;
-
-    handleClick(event){
-
-        this.showModal=true;
-    }
-    cancelHandler(event){
-        this.showModal=false; 
-        console.log(event.detail);
-        
-    }
-
-    
+  handleClick(event) {
+    this.showModal = true;
+  }
+  cancelHandler(event) {
+    this.showModal = false;
+    console.log(event.detail);
+  }
 }
